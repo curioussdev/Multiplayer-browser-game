d2dd3e938bea3c2008938297dbc4051c9031740a
Jose Lopes <946393361bill@gmail.com>
2021-07-13 02:37:52 +0100

    iniciando rotas

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+//dependencies
 var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var port = 8080;
+
+//instancing
+var app = express;
+var server =http.Server(app);
+var io = socketIO(server);
+//app.set('port', port)
+
+server.listen(port, function() {
+    console.log('im listening')
+});
+
+app.get("/", function(req, res) {
+    res.senFile();
+});
